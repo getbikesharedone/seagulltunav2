3b9d56bef05a24d56643ba530e9764baacd56e3c
josh <josh@zool.com>
2017-08-22 02:40:31 +1000

    change maps to google maps.

---
+++ frontend/www/styles.js
@@ -0,0 +1,67 @@
+var styles = [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [{"saturation": 35},{"color": "#93a1a1"},{"lightness": 0}]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [{"visibility": "on"},{"color":"#1c1c1c"},{"lightness": 0}]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [{"visibility": "off"}]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "#000000"},{"lightness": 21}]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [{"color": "#000000"},{"lightness": 17},{"weight": 1.2}]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [{"color": "#000000"},{"lightness": 21}]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [{"color": "#000000"},{"lightness": 22}]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "#000000"},{"lightness": 20}]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [{"color": "#586e75"},{"lightness": 0},{"weight": 0.2}]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [{"color": "#000000"},{"lightness": 18}]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [{"color": "#000000"},{"lightness": 15}]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [{"color": "#000000"},{"lightness": 19}]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{"color": "#262626"},{"lightness": 0}]
+    }
+]
